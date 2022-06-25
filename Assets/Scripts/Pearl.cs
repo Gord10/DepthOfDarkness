c1d9ab638d9f244e964c3f06863013711c651417
Gord10 <akk13us@yahoo.com>
2022-06-25 23:22:19 +0300

    Tween effect for pearl tint color

--- Assets/Scripts/Pearl.cs
+++ Assets/Scripts/Pearl.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Rendering.Universal;
 
 public class Pearl : MonoBehaviour
 {
+    public Color darkestTweenColor = new Color(0.6f, 0.6f, 0.6f);
     private Collider2D collider;
     private SpriteRenderer spriteRenderer;
     private AudioSource audioSource;
+    private Light2D light;
 
     private void Awake()
     {
         collider = GetComponent<Collider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         audioSource = GetComponent<AudioSource>();
+        light = GetComponentInChildren<Light2D>();
+        spriteRenderer.DOColor(darkestTweenColor, 1f).SetLoops(-1, LoopType.Yoyo);
     }
 
     public void GetCollected()
     {
         collider.enabled = false;
         spriteRenderer.enabled = false;
+        spriteRenderer.DOKill();
         audioSource.Play();
+        light.enabled = false;
         GameManager.Instance.OnFindingPearl();
     }
 }
